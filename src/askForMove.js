482be63e564edc1bd487600feb6cd87e8b06f0b9
Cristobal Jones <cjones2@uc.cl>
2021-04-02 19:17:27 -0300

    Better Functional Parser

--- src/askForMove.js
+++ src/askForMove.js
@@ -2,21 +2,22 @@ const prompt = require('prompt-sync')({ sigint: true });
 
 const askForMove = (play, jugador) => {
   const move = prompt(`Ingrese Jugada de ${jugador}: `);
-  const formattedMove = move.match(/[0-9]{1,2},[0-9]{1,2}|DB|SB|null/gi)
-  .map((x) => {
-    if (x.includes("null")){
-      return null
-    }
-    else if (x.includes(',')) {
-      return x.split(',').map(y => parseInt(y))
-    }
-    else {
-      return x
-    }
-  });
-
+  const formattedMove = 
+    move
+      .replace(/\s/g, "")
+      .match(/[0-9]{1,2},[0-9]{1,2}|DB|SB|null/gi)
+      .map(x => {
+        if (x.includes("null")){
+          return null
+        }
+        else if (x.includes(',')) {
+          return x.split(',').map(y => parseInt(y))
+        }
+        else {
+          return x
+        }
+      });
   return play(formattedMove);
-};
-
+  };
 
 module.exports = { askForMove };
