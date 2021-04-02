b0ecdf9f4081a94cf8be85a42aca2b6724238265
Cristobal Jones <cjones2@uc.cl>
2021-04-02 19:04:14 -0300

    Functional Parser

--- src/askForMove.js
+++ src/askForMove.js
@@ -2,10 +2,21 @@ const prompt = require('prompt-sync')({ sigint: true });
 
 const askForMove = (play, jugador) => {
   const move = prompt(`Ingrese Jugada de ${jugador}: `);
-  // TODO: hacer un parser recursivo
-  const formattedMove = JSON.parse(move);
+  const formattedMove = move.match(/[0-9]{1,2},[0-9]{1,2}|DB|SB|null/gi)
+  .map((x) => {
+    if (x.includes("null")){
+      return null
+    }
+    else if (x.includes(',')) {
+      return x.split(',').map(y => parseInt(y))
+    }
+    else {
+      return x
+    }
+  });
 
   return play(formattedMove);
 };
+
 
 module.exports = { askForMove };
